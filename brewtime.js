853388046b1531491c30a22af6cddd0a603d8ef8
Leona Hu Hudelson <hqleona@gmail.com>
2020-05-04 00:01:54 -0700

    initial commit

---
+++ brewtime.js
@@ -0,0 +1,56 @@
+
+let seconds = 240; // 4 mins
+let countdown = 0; // set/clear interval
+let isPaused = true;
+
+
+
+const timerDisplay = document.querySelector(".timerDisplay");
+const startButton = document.querySelector(".startButton");
+
+const alarm = document.createElement('audio'); 
+alarm.setAttribute("src", "https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3");
+
+
+// event listener for start  
+startButton.addEventListener('click', () => {
+    clearInterval(countdown);
+    isPaused = !isPaused;
+    if (!isPaused) {
+        countdown = setInterval(timer, 1000);
+    }
+    
+});
+
+//event listener for reset
+
+
+//Timer countdown
+
+function timer(){
+    seconds--; 
+    if (seconds < 0 ) {
+        clearInterval(countdown);
+        alarm.currentTime = 0;
+        alarm.play();
+        countdown = setInterval(timer, 1000);
+    }
+}
+
+//Countdown display in HTML
+
+function countdownDisplay() {
+    let minutes = Math.floor(seconds/60);
+    let remainderSeconds = seconds % 60;
+    timerDisplay.innerHTML = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+}
+
+function updateHTML() {
+    countdownDisplay();
+    
+
+}
+
+window.setInterval(updateHTML, 100);
+
+document.onclick = updateHTML;
